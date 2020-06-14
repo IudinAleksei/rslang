c5e228c24d5cf92a02a16beffd0e65b6cd24357f
Alexey_Yudin <aviordream@gmail.com>
2020-06-14 12:26:42 +0300

    feat: add project structure and config files: editiorconfig, eslintrc, stylelintrc

---
+++ webpack.config.js
@@ -0,0 +1,113 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+module.exports = {
+  entry: {
+    main: [
+      '@babel/polyfill',
+      './src/index.js',
+      './src/sass/index.scss',
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: isDev ? 'source-map' : '',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  devServer: {
+    port: 8080,
+    hot: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      minify: isProd,
+      favicon: './src/favicon.ico',
+    }),
+    new HTMLWebpackPlugin({
+      template: './src/primary.html',
+      filename: 'primary.html',
+      minify: isProd,
+      favicon: './src/favicon.ico',
+    }),
+    new CopyWebpackPlugin([
+      { from: './src/assets', to: 'assets' },
+    ]),
+    new MiniCssExtractPlugin({
+      filename: isDev ? 'style.css' : 'style.[hash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(woff(2)?|ttf)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(webp|png|svg|jpe?g|gif)(\?v=\d+\.\d+\.\d+)?$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
+            },
+          },
+          'css-loader',
+          'sass-loader'],
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
