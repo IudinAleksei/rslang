fd938597e836e9e10b8d47b3ea5069d44d01fbb6
igorzima <redteam122@gmail.com>
2020-06-19 21:18:12 +0300

    refactor: moved githubMedia from network to utils

--- src/app/common/index.js
+++ src/app/common/index.js
@@ -1,4 +1,4 @@
-import getMedia from './network/githubMedia/githubMedia';
+import getMedia from './utils/githubMedia/githubMedia';
 
 export { getWords,  getCountWords,  getWordById,  createUser,  loginUser,  deleteUser,  createUserWord,  getAllUserWords,  getUserWordById,  deleteUserWordById,  updateUserWord,  getUserStatistic,  upsertUserStatistic,  getUserSettings,  upsertUserSettings } from './network/backendWords/backendWords';
 
--- src/app/common/network/githubMedia/githubMedia.js
+++
@@ -1,5 +0,0 @@
-export default async function getMedia(path) {
-  const responce = await fetch(`https://raw.githubusercontent.com/IudinAleksei/rslang-data/master/${path}`);
-
-  return responce;
-}
