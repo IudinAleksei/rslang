fd068d7d152d002791d74c01724277a35fd193a5
katerynaprokofieva <gucolk@gmail.com>
2020-06-21 00:18:27 +0300

    add fixes to header

--- src/app/main/menu/menu.js
+++ src/app/main/menu/menu.js
@@ -1,14 +1,14 @@
 // burger menu handling
 function onClickBurgerIcon() {
-  document.querySelector('.menu__icon').classList.toggle('menu-active');
-  document.querySelector('.menu-bg').classList.toggle('menu__bg-active');
-  document.querySelector('.menu__items').classList.toggle('menu-active');
+  document.querySelector('.menu__icon').classList.toggle('menu_active');
+  document.querySelector('.menu-bg').classList.toggle('menu__bg_active');
+  document.querySelector('.menu__items').classList.toggle('menu_active');
 }
 
 function removeActiveStateFromBurger() {
-  document.querySelector('.menu-bg').classList.remove('menu__bg-active');
-  document.querySelector('.menu__icon').classList.remove('menu-active');
-  document.querySelector('.menu__items').classList.remove('menu-active');
+  document.querySelector('.menu-bg').classList.remove('menu__bg_active');
+  document.querySelector('.menu__icon').classList.remove('menu_active');
+  document.querySelector('.menu__items').classList.remove('menu_active');
 }
 
 function onClickOutsideMenu(event) {
