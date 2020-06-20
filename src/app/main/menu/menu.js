cc4ce5dfb926dca3a7dbb48222f5e22c15b85ded
katerynaprokofieva <gucolk@gmail.com>
2020-06-20 23:53:57 +0300

    add fixes to header

--- src/app/main/menu/menu.js
+++ src/app/main/menu/menu.js
@@ -1,40 +1,38 @@
-const menu = document.querySelector('.menu__icon');
-const menuItems = document.querySelector('.menu__items');
-const menuBg = document.querySelector('.menu-bg');
-const selectedMenuElement = document.getElementById('menu');
-const menuItem = document.querySelector('.menu__items__item');
+// burger menu handling
+function onClickBurgerIcon() {
+  document.querySelector('.menu__icon').classList.toggle('menu-active');
+  document.querySelector('.menu-bg').classList.toggle('menu__bg-active');
+  document.querySelector('.menu__items').classList.toggle('menu-active');
+}
+
+function removeActiveStateFromBurger() {
+  document.querySelector('.menu-bg').classList.remove('menu__bg-active');
+  document.querySelector('.menu__icon').classList.remove('menu-active');
+  document.querySelector('.menu__items').classList.remove('menu-active');
+}
+
+function onClickOutsideMenu(event) {
+  const isClickInside = document.getElementById('menu').contains(event.target);
+  if (!isClickInside) {
+    removeActiveStateFromBurger();
+  }
+}
+
+function onMenuItemClick(event) {
+  const sectionInTheMenu = event.target.closest('.menu__items__item');
+  if (sectionInTheMenu) {
+    document.querySelector('.menu__items').querySelectorAll('.menu__items__item').forEach((element) => element.classList.remove('menu__items__item_active'));
+    sectionInTheMenu.classList.add('menu__items__item_active');
+    removeActiveStateFromBurger();
+  }
+}
 
 export default function menuHandling() {
-  menuItem.classList.add('menu__active-element'); // highlight first item in the menu
+  const menu = document.querySelector('.menu__icon');
+  const menuItems = document.querySelector('.menu__items');
+  const menuItem = document.querySelector('.menu__items__item');
 
-  // burger menu handling
-  function onClickBurgerIcon() {
-    menu.classList.toggle('menu-active');
-    menuBg.classList.toggle('menu__bg-active');
-    menuItems.classList.toggle('menu-active');
-  }
-
-  function removeActiveStateFromBurger() {
-    menuBg.classList.remove('menu__bg-active');
-    menu.classList.remove('menu-active');
-    menuItems.classList.remove('menu-active');
-  }
-
-  function onClickOutsideMenu(event) {
-    const isClickInside = selectedMenuElement.contains(event.target);
-    if (!isClickInside) {
-      removeActiveStateFromBurger();
-    }
-  }
-
-  function onMenuItemClick(event) {
-    const sectionInTheMenu = event.target.closest('.menu__items__item');
-    if (sectionInTheMenu) {
-      menuItems.querySelectorAll('a').forEach((element) => element.classList.remove('menu__active-element'));
-      sectionInTheMenu.classList.add('menu__active-element');
-      removeActiveStateFromBurger();
-    }
-  }
+  menuItem.classList.add('menu__items__item_active'); // highlight first item in the menu
 
   menuItems.addEventListener('click', onMenuItemClick);
 
