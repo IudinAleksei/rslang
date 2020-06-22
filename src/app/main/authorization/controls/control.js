0668b37d7548802c2898de999ad3e5482cef14db
cup0ra <cup0ra@users.noreply.github.com>
2020-06-22 21:30:51 +0300

    fix: fix control.js

--- src/app/main/authorization/controls/control.js
+++ src/app/main/authorization/controls/control.js
@@ -1,21 +1,34 @@
 import { loginUser, createUser } from '../../../common/index';
 
-let email = '';
-let password = '';
+function showError() {
+  const errors = document.createElement('div');
+  errors.className = 'error';
+  errors.innerHTML = '<p class="error-massage">Failed to register</p>';
+  document.querySelector('body').append(errors);
+  setTimeout(() => {
+    document.querySelector('body').lastElementChild.remove();
+  }, 4000);
+}
 
-function getSign() {
+function getSign(email, password) {
   if (document.getElementById('signin').checked) {
-    loginUser({ email: `${email}`, password: `${password}` });
+    loginUser({ email: `${email}`, password: `${password}` }).then((data) => data.token);
   }
   if (document.getElementById('signup').checked) {
-    createUser({ email: `${email}`, password: `${password}` });
+    createUser({ email: `${email}`, password: `${password}` }).then((data) => {
+      if (data === null) {
+        showError();
+      } else {
+        loginUser({ email: `${email}`, password: `${password}` }).then((result) => result.token);
+      }
+    });
   }
 }
 export default function formHandling() {
   document.getElementById('authorization').addEventListener('submit', (event) => {
     event.preventDefault();
-    email = document.getElementById('email').value;
-    password = document.getElementById('password').value;
-    getSign();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    getSign(email, password);
   });
 }
--- src/app/main/authorization/index.js
+++
@@ -1,5 +0,0 @@
-import { showForm } from './view/showForm';
-import formHandling from './controls/control';
-
-export { showForm };
-export { formHandling };
