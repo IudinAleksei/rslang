93a9c481de151e8b657e185bb8404341430154ca
katerynaprokofieva <gucolk@gmail.com>
2020-06-23 19:53:36 +0300

    add menu handler

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import menuHandling from './app/main/menu/menu';
-// import renderSettings from './app/main/settings/index';
+import renderSettings from './app/main/settings/index';
 import renderMenuItem from './app/common/controls/controls';
 
 window.onload = () => {
   menuHandling();
-  // renderSettings(); // this is the first page user sees after login
+  renderSettings(); // this is the first page user sees after login
   renderMenuItem();
 };
