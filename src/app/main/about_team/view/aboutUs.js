cd9ac9f4075171ccdb27281ccabd994bd937c994
Anna <a.sobolenko1194@gmail.com>
2020-06-24 23:09:14 +0300

    fix: function

--- src/app/main/about_team/view/aboutUs.js
+++ src/app/main/about_team/view/aboutUs.js
@@ -1,40 +1,40 @@
 const param = [{
-  img: 'src/assets/img/about-us/cat.jpg',
+  img: 'assets/img/about-us/cat.jpg',
   name: 'Iudin Aleksei',
   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
   telegramLink: 'https://telegram.org/',
   githubLink: 'https://github.com/',
 },
 {
-  img: 'src/assets/img/about-us/cat.jpg',
+  img: 'assets/img/about-us/cat.jpg',
   name: 'Kate Prokofieva',
   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
   telegramLink: 'https://telegram.org/',
   githubLink: 'https://github.com/',
 },
 {
-  img: 'src/assets/img/about-us/cat.jpg',
+  img: 'assets/img/about-us/cat.jpg',
   name: 'Searhei Baranenkau',
   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
   telegramLink: 'https://telegram.org/',
   githubLink: 'https://github.com/',
 },
 {
-  img: 'src/assets/img/about-us/cat.jpg',
+  img: 'assets/img/about-us/cat.jpg',
   name: 'Ihar Zimnitski',
   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
   telegramLink: 'https://telegram.org/',
   githubLink: 'https://github.com/',
 },
 {
-  img: 'src/assets/img/about-us/cat.jpg',
+  img: 'assets/img/about-us/cat.jpg',
   name: 'Semen Shpakau',
   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
   telegramLink: 'https://telegram.org/',
   githubLink: 'https://github.com/',
 },
 {
-  img: 'src/assets/img/about-us/cat.jpg',
+  img: 'assets/img/about-us/cat.jpg',
   name: 'Hanna Tsitsenkova',
   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
   telegramLink: 'https://telegram.org/',
@@ -42,14 +42,16 @@ const param = [{
 },
 ];
 
-const card = (img, name, text, telegramLink, githubLink) => `<img src="${img}" class="photo">
-<div class="wrap-name">
-  <p class="name">${name}</p>
-  <p class="text">${text}</p>
-</div>
-<div class="contact">
-  <a href="${telegramLink}"><img src="file:///G:/assets/telegram-icon.png" class="telegtam"></a>
-  <a href="${githubLink}"><img src="file:///G:/assets/github-icon.jpg" class="github"></a>
+const card = (img, name, text, telegramLink, githubLink) => `<div class="about-us__wrap">
+  <img src="${img}" class="photo">
+  <div class="wrap-name">
+    <p class="name">${name}</p>
+    <p class="text">${text}</p>
+  </div>
+  <div class="contact">
+    <a href="${telegramLink}"><img src="file:///G:/assets/telegram-icon.png" class="telegtam"></a>
+    <a href="${githubLink}"><img src="file:///G:/assets/github-icon.jpg" class="github"></a>
+  </div>
 </div>`;
 
 const logo = '<p class="logo-about-us">About us</p>';
@@ -59,13 +61,14 @@ const generatePageAboutUs = () => {
   divAboutUs.className = 'main__about-us';
 
   document.querySelector('.main-container').appendChild(divAboutUs);
-  divAboutUs.insertAdjacentHTML('beforebegin', logo);
+  divAboutUs.insertAdjacentHTML('afterbegin', logo);
 
   const divWrapCards = document.createElement('div');
   divWrapCards.className = 'wrap-cards';
   divAboutUs.appendChild(divWrapCards);
 
   for (let i = 0; i < param.length; i += 1) {
-    divWrapCards.insertAdjacentHTML('beforebegin', card(param[i].img, param[i].name, param[i].text, param[i].telegramLink, param[i].githubLink));
+    divWrapCards.insertAdjacentHTML('afterbegin', card(param[i].img, param[i].name, param[i].text, param[i].telegramLink, param[i].githubLink));
   }
 };
+export default generatePageAboutUs;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import menuHandling from './app/main/menu/menu';
+import generatePageAboutUs from './app/main/about_team/view/aboutUs';
 
 window.onload = () => {
   menuHandling();
+  generatePageAboutUs();
 };
