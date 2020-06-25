d00ebe06c05a04facaa8b2844bc85c48fcd819c8
Alexey_Yudin <aviordream@gmail.com>
2020-06-25 22:10:56 +0300

    feat: add three localStorage functions and add order sort for style

--- src/app/common/utils/localStorage.js
+++ src/app/common/utils/localStorage.js
@@ -12,3 +12,32 @@ const setDefaultLocalData = () => {
     window.localStorage.setItem(...defEntry);
   });
 };
+
+export const setLocalData = (data = {}) => {
+  if (data) {
+    const dataArray = Object.entries(data);
+    dataArray.forEach((entry) => {
+      window.localStorage.setItem(...entry);
+    });
+  }
+  return !!data;
+};
+
+export const getLocalData = () => {
+  const dataNameArray = Object.keys(DEFAULT_LOCAL_DATA);
+  const result = dataNameArray.reduce((acc, param) => {
+    const entry = {};
+    entry[param] = window.localStorage.getItem(param);
+    return Object.assign(acc, entry);
+  }, {});
+  return result;
+};
+
+export const getAndInitLocalData = () => {
+  if (!hasSavedLocalData()) {
+    setDefaultLocalData();
+  }
+
+  const sessionData = getLocalData();
+  return sessionData;
+};
