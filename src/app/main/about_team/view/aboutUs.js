537440d6ded4817df6ad7e19c8938d3c05707356
Anna <a.sobolenko1194@gmail.com>
2020-06-26 17:21:46 +0300

    code reviewed  fixes

--- src/app/main/about_team/view/aboutUs.js
+++ src/app/main/about_team/view/aboutUs.js
@@ -1,74 +1,71 @@
-const param = [{
-  img: 'assets/img/about-us/cat.jpg',
-  name: 'Iudin Aleksei',
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
-  telegramLink: 'https://telegram.org/',
-  githubLink: 'https://github.com/',
-},
-{
-  img: 'assets/img/about-us/cat.jpg',
-  name: 'Kate Prokofieva',
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
-  telegramLink: 'https://telegram.org/',
-  githubLink: 'https://github.com/',
-},
-{
-  img: 'assets/img/about-us/cat.jpg',
-  name: 'Searhei Baranenkau',
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
-  telegramLink: 'https://telegram.org/',
-  githubLink: 'https://github.com/',
-},
-{
-  img: 'assets/img/about-us/cat.jpg',
-  name: 'Ihar Zimnitski',
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
-  telegramLink: 'https://telegram.org/',
-  githubLink: 'https://github.com/',
-},
-{
-  img: 'assets/img/about-us/cat.jpg',
-  name: 'Semen Shpakau',
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
-  telegramLink: 'https://telegram.org/',
-  githubLink: 'https://github.com/',
-},
-{
-  img: 'assets/img/about-us/cat.jpg',
-  name: 'Hanna Tsitsenkova',
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
-  telegramLink: 'https://telegram.org/',
-  githubLink: 'https://github.com/',
-},
-];
-
-const card = (img, name, text, telegramLink, githubLink) => `<div class="about-us__wrap">
-  <img src="${img}" class="photo">
-  <div class="wrap-name">
-    <p class="name">${name}</p>
-    <p class="text">${text}</p>
-  </div>
-  <div class="contact">
-    <a href="${telegramLink}"><img src="assets/img/about-us/telegram.svg" class="telegtam"></a>
-    <a href="${githubLink}"><img src="assets/img/about-us/logo.svg" class="github"></a>
-  </div>
-</div>`;
-
-const logo = '<p class="logo-about-us">About us</p>';
-
 const generatePageAboutUs = () => {
-  const divAboutUs = document.createElement('div');
-  divAboutUs.className = 'main__about-us';
+  const OUR_TEAM = [{
+    img: 'assets/img/about-us/cat.jpg',
+    name: 'Aleksei Iudin',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
+    telegramLink: 'https://telegram.org/',
+    githubLink: 'https://github.com/',
+  },
+  {
+    img: 'assets/img/about-us/cat.jpg',
+    name: 'Hanna Tsitsenkova',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
+    telegramLink: 'https://telegram.org/',
+    githubLink: 'https://github.com/',
+  },
+  {
+    img: 'assets/img/about-us/cat.jpg',
+    name: 'Ihar Zimnitski',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
+    telegramLink: 'https://telegram.org/',
+    githubLink: 'https://github.com/',
+  },
+  {
+    img: 'assets/img/about-us/cat.jpg',
+    name: 'Searhei Baranenkau',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
+    telegramLink: 'https://telegram.org/',
+    githubLink: 'https://github.com/',
+  },
+  {
+    img: 'assets/img/about-us/cat.jpg',
+    name: 'Semen Shpakau',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
+    telegramLink: 'https://telegram.org/',
+    githubLink: 'https://github.com/',
+  },
+  {
+    img: 'assets/img/about-us/cat.jpg',
+    name: 'Kate Prokofieva',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum libero eu quam cursus dapibus eget non augue. Morbi pulvinar purus non sodales eleifend.',
+    telegramLink: 'https://telegram.org/',
+    githubLink: 'https://github.com/',
+  },
+  ];
+  const CARD = (img, name, text, telegramLink, githubLink) => `<div class="about-us__wrap">
+              <img src="${img}" class="photo">
+              <div class="wrap-name">
+                <p class="name">${name}</p>
+                <p class="text">${text}</p>
+              </div>
+              <div class="contact">
+                <a href="${telegramLink}"><img src="assets/img/about-us/telegram.svg" class="telegtam"></a>
+                <a href="${githubLink}"><img src="assets/img/about-us/logo.svg" class="github"></a>
+              </div>
+            </div>`;
+  const LOGO = '<p class="logo-about-us">About us</p>';
+  const DIV_ABOUT_US = document.createElement('div');
+  DIV_ABOUT_US.className = 'main__about-us';
 
-  document.querySelector('.main-container').appendChild(divAboutUs);
-  divAboutUs.insertAdjacentHTML('afterbegin', logo);
+  document.querySelector('.main-container').appendChild(DIV_ABOUT_US);
+  DIV_ABOUT_US.insertAdjacentHTML('afterbegin', LOGO);
 
-  const divWrapCards = document.createElement('div');
-  divWrapCards.className = 'wrap-cards';
-  divAboutUs.appendChild(divWrapCards);
+  const DIV_WRAP_CARDS = document.createElement('div');
+  DIV_WRAP_CARDS.className = 'wrap-cards';
+  DIV_ABOUT_US.appendChild(DIV_WRAP_CARDS);
 
-  for (let i = 0; i < param.length; i += 1) {
-    divWrapCards.insertAdjacentHTML('afterbegin', card(param[i].img, param[i].name, param[i].text, param[i].telegramLink, param[i].githubLink));
+  for (let i = 0; i < OUR_TEAM.length; i += 1) {
+    DIV_WRAP_CARDS.insertAdjacentHTML('beforebegin', CARD(OUR_TEAM[i].img, OUR_TEAM[i].name, OUR_TEAM[i].text, OUR_TEAM[i].telegramLink, OUR_TEAM[i].githubLink));
   }
 };
 export default generatePageAboutUs;
