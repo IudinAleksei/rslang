0f3c97bd27dea80e5991b39665d0a3a556774a40
katerynaprokofieva <gucolk@gmail.com>
2020-06-30 11:10:36 +0300

    add click handler

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import menuHandling from './app/main/menu/menu';
+import menuClickHandler from './app/common/controls/controls';
 
 window.onload = () => {
   menuHandling();
+  menuClickHandler();
 };
