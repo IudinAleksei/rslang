2a1a5aead64b69727bdec22deab1f58c49cc3bbf
Anna <a.sobolenko1194@gmail.com>
2020-07-09 00:09:32 +0300

    fix: main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import menuHandling from './app/main/menu/menu';
-// import startMain from './app/common/controls/controls';
-// import { showForm, formHandling } from './app/main/authorization/index';
-import createCardWord from './app/main/training/index';
+import startMain from './app/common/controls/controls';
+import { showForm, formHandling } from './app/main/authorization/index';
 
 window.onload = () => {
   menuHandling();
-  // showForm();
-  // formHandling(startMain);
-  createCardWord();
+  showForm();
+  formHandling(startMain);
 };
