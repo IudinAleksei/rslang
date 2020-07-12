47dad50f681437425cc06bfbd705d591323821d8
Anna <a.sobolenko1194@gmail.com>
2020-07-12 13:24:31 +0300

    fix: audiochallenge game: index.js, _game.scss, _startPage.scss; main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import menuHandling from './app/main/menu/menu';
 import startMain from './app/common/controls/controls';
 import { showForm, formHandling } from './app/main/authorization/index';
 
-
 window.onload = () => {
   menuHandling();
   showForm();
